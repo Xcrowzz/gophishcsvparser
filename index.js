9e778dc0a4309c570ddeae35ec03298d85820a99
Xcrowzz <xcrowzz@protonmail.com>
2021-01-05 02:39:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const csvParser = require("csv-parse");
+const p = require("ua-parser");
+const fs = require("fs");
+
+const csvToBuff = () => {
+	return new Promise((resolve, reject) => {
+		const buff = [];
+		fs.createReadStream('data.csv').pipe(csvParser()).on('data', (row) => {
+			buff.push(row);
+		}).on('error', () => {
+			console.log('Something fucky just happened.');
+			reject();
+		}).on('end', () => {
+			console.log('CSV file successfully processed');
+			resolve(buff);
+		});
+	});
+}
+
+const onlyUnique = (value, index, self) => {
+	return self.indexOf(value) === index;
+}
+
+const clearDupes = (hitMap) => {
+	const idMap = hitMap.map((e) => e[0]);
+
+	const unique = idMap.filter(onlyUnique);
+	console.log(unique);
+	return unique;
+}
+
+const cleanBuff = (buff) => {
+	return new Promise(async (resolve) => {
+		buff.splice(0, 2);
+		const totalHits = buff.filter((entry) => entry[3] === 'Clicked Link' && entry[4] !== '');
+		const hitMap = totalHits
+			.map((e) => [e[1], e[4]])
+			.filter((entry) => (p.parse(entry[1]).os.family === 'Android' || p.parse(entry[1]).device.family === 'iPhone'));
+		resolve(clearDupes(hitMap));
+	});
+}
+
+const start = () => {
+
+	csvToBuff().then((buff) => {
+		cleanBuff(buff).then((buff) => {
+			console.log(`New buffer length: ${buff.length}`);
+		})
+	}).catch((e) => {
+		console.error(`Nope: ${e}`);
+	});
+}
+
+
+start();
